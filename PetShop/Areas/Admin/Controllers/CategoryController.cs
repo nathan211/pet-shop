298c4645d863fe4ed160b714748abcd00898e55b
Nathan <vietphi99dh@gmail.com>
2020-06-03 01:23:32 +0700

    complete CRUD admin side

--- PetShop/Areas/Admin/Controllers/CategoryController.cs
+++ PetShop/Areas/Admin/Controllers/CategoryController.cs
@@ -21,13 +21,81 @@ namespace PetShop.Areas.Admin.Controllers
         {
             var categories = db.Categories.ToList();
             ViewBag.ParentId = new SelectList(db.ParentCategories.ToList(), "Id", "Name");
+            ViewBag.PetId = new SelectList(db.Pets.ToList(), "Id", "Name");
             return View(categories);
         }
 
         public ActionResult AddNew()
         {
             ViewBag.ParentId = new SelectList(db.ParentCategories.ToList(), "Id", "Name");
+            ViewBag.PetId = new SelectList(db.Pets.ToList(), "Id", "Name");
             return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult AddNew(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.InsertOnSubmit(category);
+                db.SubmitChanges();
+                return RedirectToAction("ListAllCategories");
+            }
+            return this.AddNew();
+        }
+
+        [HttpPost]
+        public JsonResult CategoryInfo(long id)
+        {
+            var category = db.Categories.Where(x => x.Id == id).FirstOrDefault();
+            if(category != null)
+            {
+                return Json(new {
+                    CategoryName = category.Name,
+                    ParentId = category.ParentId,
+                    PetId = category.PetId
+                });
+            }
+            else
+            {
+                return Json("CannotFindCategory", JsonRequestBehavior.AllowGet);
+            }
+           
+        }
+
+        [HttpPost]
+        public JsonResult UpdateCategory(long id, string name, int parentId, int petId)
+        {
+            var category = db.Categories.Where(x => x.Id == id).FirstOrDefault();
+
+            if(category != null)
+            {
+                category.Name = name;
+                category.ParentId = parentId;
+                category.PetId = petId;
+                UpdateModel(category);
+                db.SubmitChanges();
+                return Json("Success", JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json("CannotFindCategory", JsonRequestBehavior.AllowGet);
+            }
+           
+        }
+
+
+        [HttpDelete]
+        public ActionResult Delete(long id)
+        {
+            var category = db.Categories.Where(x => x.Id == id).FirstOrDefault();
+            if (category != null)
+            {
+                db.Categories.DeleteOnSubmit(category);
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
